513d06b2b578e65607fa25472984bfbbff08ad2d
pavlion <iyadalagha"hotmail.com>
2022-05-09 21:08:28 +0300

    Showing test log message

--- app/src/main/java/com/lalorosas/myinvestments/MainActivity.kt
+++ app/src/main/java/com/lalorosas/myinvestments/MainActivity.kt
@@ -14,6 +14,7 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d("test","Hello World")
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
